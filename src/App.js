2d49f5bc5cd089cbfbe22a5f877fee0614366543
Barbara Olivieri <barbaranolivieri@gmail.com>
2018-02-09 19:22:56 -0200

    Add setState

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+	state = {
+		persons: [
+		{ name: 'Max', age: 28},
+		{ name: 'Manu', age: 26},
+		{ name: 'Barbs', age: 23}
+
+		],
+		otherState: 'some other value'
+	}
+
+	switchNameHandler = () => {
+		// this.state.person[0].name = "Maximilian";
+		this.setState({
+			persons: [
+		{ name: 'Maximilian', age: 28},
+		{ name: 'Manu', age: 26},
+		{ name: 'Barbs', age: 25}
+			]
+
+		});
+	}
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Im A React App</h1>
-      <Person />
-      <Person />
-      <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
 
     );
