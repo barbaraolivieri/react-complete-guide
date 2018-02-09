a7d1d7256ee23315b98dcb66092cf2f8cc307c15
Barbara Olivieri <barbaranolivieri@gmail.com>
2018-02-09 21:51:34 -0200

    Delete e lists

--- src/App.js
+++ src/App.js
@@ -15,16 +15,11 @@ class App extends Component {
 		showPersons: false
 	}
 
-	switchNameHandler = (newName) => {
-		// this.state.person[0].name = "Maximilian";
-		this.setState({
-			persons: [
-		{ name: newName, age: 28},
-		{ name: 'Manu', age: 26},
-		{ name: 'Barbs', age: 25}
-			]
-
-		});
+	deletePersonHandler = (personIndex) => {
+		// const persons = this.state.persons.slice();
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({persons: persons});
 	}
 
 	nameChangedHandler = (event) => {
@@ -60,17 +55,13 @@ class App extends Component {
   	if(this.state.showPersons) {
   		persons = (
   			<div>
-			      <Person 
-			      name={this.state.persons[0].name}
-			       age={this.state.persons[0].age} />
-			      <Person
-			       name={this.state.persons[1].name}
-			        age={this.state.persons[1].age}
-			        click={this.switchNameHandler.bind(this, '2')}
-			        changed={this.nameChangedHandler}> My Hobbies: Racing</Person>
-			      <Person 
-			      name={this.state.persons[2].name} 
-			      age={this.state.persons[2].age} />
+  				{this.state.persons.map((person,index) => {
+  					return <Person 
+  					click={ () => this.deletePersonHandler(index)}
+  					name={person.name} 
+  					age={person.age} 
+  					/>
+  				})}
 		      </div>
   		);
 
